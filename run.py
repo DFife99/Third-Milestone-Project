1ff3435c724fc38ca1d55f77d36e8a8ce8d1a33d
DFife99 <Devlan.fife2@gmail.com>
2020-09-23 12:33:48 +0000

    added python file, styled the nav, started creating the javascipt to continue styling the nav bar

--- run.py
+++ run.py
@@ -1,2 +1,36 @@
 import os
-import flask from Flask
+from flask import Flask, render_template, request, flash
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/phones')
+def phones():
+    return render_template("phones.html")
+
+
+@app.route('/tablets')
+def tablets():
+    return render_template("tablets.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
+@app.route('/store')
+def store():
+    return render_template("store.html")
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
